b719ec3f7015e1a98b78db7085ec8d6abd0b03e3
Stephan Spielmann <stephan@bazzinga.eu>
2020-08-06 23:56:39 +0200

    created a map-view with dart-lang; no data yet

--- lsa-karten-darmstadt/README.md
+++ lsa-karten-darmstadt/README.md
@@ -8,4 +8,6 @@
 ## Development diary
 * installed and then wrote "how to build and run?" in the this README.md
 * write this while waiting for webdev to complete, example app (looks like a TODO-app) seems to work
-
+* removed example and created new `stagehand web-simple`
+* added dartleaf ^0.1.5, added the minimal required code in main.dart, added leaflet to the index.html, set heigth of the map-div to 100%
+* will commit now as first milestone (getting a map loaded in the browser) is achieved
--- lsa-karten-darmstadt/web/main.dart
+++ lsa-karten-darmstadt/web/main.dart
@@ -1,6 +1,21 @@
-import 'package:angular/angular.dart';
-import 'package:lsa_karten_darmstadt/app_component.template.dart' as ng;
+import 'dart:html';
+import 'package:dartleaf/dartleaf.dart';
 
 void main() {
-  runApp(ng.AppComponentNgFactory);
+//  var output = querySelector('#output');
+//  output.text = 'Your Dart app is running.';
+//  output.children.addAll(thingsTodo().map(newLI));
+  var mapElement = querySelector('#output');
+  var options = TileLayerOptions();
+  var urlTemplate = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var map = LeafletMap.fromElement(mapElement);
+  TileLayer(urlTemplate, options).addTo(map);
+  map.setView(LatLng(49.8680, 8.655), 12);
 }
+
+//LIElement newLI(String itemText) => LIElement()..text = itemText;
+
+//Iterable<String> thingsTodo() sync* {
+//  for (var s in ['a', 'b', 'c']) yield s;
+//  yield "blub";
+//}
